a8186e452cc9afd58da3a08d1a2b733ff3909178
acevedo <acevedo@mit.edu>
2025-03-05 03:21:54 -0500

    another change to links

--- static/global.js
+++ static/global.js
@@ -3,10 +3,10 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: ".", title: "Home" },
-  { url: "my-portfolio/projects", title: "Projects" },
-  { url: "my-portfolio/resume", title: "Resume" },
-  { url: "my-portfolio/contact", title: "Contact" },
+  { url: "./my-portfolio", title: "Home" },
+  { url: "./my-portfolio/projects", title: "Projects" },
+  { url: "./my-portfolio/resume", title: "Resume" },
+  { url: "./my-portfolio/contact", title: "Contact" },
   { url: "https://github.com/mass-ave", title: "GitHub" },
 ];
 
