cb655b3b949066e5e68d9c59d7c7b8b810d033c6
acevedo <acevedo@mit.edu>
2025-03-05 03:13:44 -0500

    changed trailing slash

--- static/global.js
+++ static/global.js
@@ -3,10 +3,10 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: "./", title: "Home" },
-  { url: "./projects", title: "Projects" },
-  { url: "./resume", title: "Resume" },
-  { url: "./contact", title: "Contact" },
+  { url: ".", title: "Home" },
+  { url: "projects", title: "Projects" },
+  { url: "resume", title: "Resume" },
+  { url: "contact", title: "Contact" },
   { url: "https://github.com/mass-ave", title: "GitHub" },
 ];
 
