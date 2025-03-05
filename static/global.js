94bfaad25a3305c1a9e16ac355c3fea6667accff
acevedo <acevedo@mit.edu>
2025-03-05 16:27:15 -0500

    removed are we home

--- static/global.js
+++ static/global.js
@@ -13,11 +13,10 @@ let pages = [
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  if (!ARE_WE_HOME && !url.startsWith("http")) {
+  if (!url.startsWith("http")) {
     url = `../${url}`;
   }
   let a = document.createElement("a");
