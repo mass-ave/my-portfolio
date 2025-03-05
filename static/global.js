99e190b226777b21762d93a0603cbb26c90bc4c4
acevedo <acevedo@mit.edu>
2025-03-05 03:25:48 -0500

    navbar ain't working

--- static/global.js
+++ static/global.js
@@ -3,10 +3,10 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: "./my-portfolio", title: "Home" },
-  { url: "./my-portfolio/projects", title: "Projects" },
-  { url: "./my-portfolio/resume", title: "Resume" },
-  { url: "./my-portfolio/contact", title: "Contact" },
+  { url: "./", title: "Home" },
+  { url: "./projects", title: "Projects" },
+  { url: "./resume", title: "Resume" },
+  { url: "./contact", title: "Contact" },
   { url: "https://github.com/mass-ave", title: "GitHub" },
 ];
 
