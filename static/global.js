12696b4e01f8bb118695b17060ca966ac48f1d3e
acevedo <acevedo@mit.edu>
2025-03-10 16:08:38 -0400

    lab4.2.3 done

--- static/global.js
+++ static/global.js
@@ -47,21 +47,21 @@ function $$(selector, context = document) {
 // 	</label>`
 // );
 
-let select = document.querySelector("select");
+// let select = document.querySelector("select");
 
-select.addEventListener("input", function (event) {
-  console.log("color scheme changed to", event.target.value);
-  document.documentElement.style.setProperty(
-    "color-scheme",
-    event.target.value
-  );
-  localStorage.colorScheme = event.target.value;
-});
+// select.addEventListener("input", function (event) {
+//   console.log("color scheme changed to", event.target.value);
+//   document.documentElement.style.setProperty(
+//     "color-scheme",
+//     event.target.value
+//   );
+//   localStorage.colorScheme = event.target.value;
+// });
 
-if ("colorScheme" in localStorage) {
-  document.documentElement.style.setProperty(
-    "color-scheme",
-    localStorage.colorScheme
-  );
-  select.value = localStorage.colorScheme;
-}
+// if ("colorScheme" in localStorage) {
+//   document.documentElement.style.setProperty(
+//     "color-scheme",
+//     localStorage.colorScheme
+//   );
+//   select.value = localStorage.colorScheme;
+// }
