1ffc1feae73f551b6d6bf77db3bba8ad32d913e3
acevedo <acevedo@mit.edu>
2025-03-10 18:05:44 -0400

    lab5.3 done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,63 @@
   taking Interactive Data Visualization and Society. Feel free to reach out
   to me using the <a href="./contact">Contact</a> page of the website.
 </p>
-{#await fetch("https://api.github.com/users/mass-ave") }
-<p>Loading...</p>
-{:then response} {#await response.json()}
-<p>Decoding...</p>
-{:then data}
-<p>The data is { JSON.stringify(data) }</p>
-{:catch error}
-<p class="error">Something went wrong: {error.message}</p>
-{/await} {:catch error}
-<p class="error">Something went wrong: {error.message}</p>
-{/await}
 <img src="images/sunrise.jpg" alt="sunrise of the Boston skyline" />
+{#await fetch("https://api.github.com/users/mass-ave")}
+  <p>Loading...</p>
+{:then response}
+  {#await response.json()}
+    <p>Decoding...</p>
+  {:then data}
+    <section>
+      <h2>My GitHub Stats</h2>
+      <dl>
+        <dt>Followers:</dt>
+        <dd>{data.followers}</dd>
+        <dt>Following:</dt>
+        <dd>{data.following}</dd>
+        <dt>Public Repositories:</dt>
+        <dd>{data.public_repos}</dd>
+      </dl>
+    </section>
+  {:catch error}
+    <p class="error">Something went wrong: {error.message}</p>
+  {/await}
+{:catch error}
+  <p class="error">Something went wrong: {error.message}</p>
+{/await}
 <h2>Latest Projects</h2>
 <div class="projects">
   {#each [projects[0], projects[3], projects[7]] as p}
   <Project data={p} hLevel="3" />
   {/each}
 </div>
+<style>
+  section {
+    padding: 1em;
+    border-radius: 1em;
+    max-width: 100ch;
+    margin: 1em auto;
+    text-align: center;
+  }
+
+  dl {
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 1em;
+  }
+
+  dt {
+    font-weight: bold;
+    text-align: right;
+  }
+
+  dd {
+    text-align: left;
+    margin: 0;
+  }
+
+  .error {
+    color: oklch(43.72% 0.1676 27.84);
+    font-weight: bold;
+  }
+</style>
