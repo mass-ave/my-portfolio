1cb1daa4e4bc04c68500beef5fe96949fb0d3115
acevedo <acevedo@mit.edu>
2025-03-10 16:01:55 -0400

    bug hunting moment lol

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,85 @@
+<script>
+    import { page } from "$app/stores";
+    let pages = [
+        { url: "./", title: "Home" },
+        { url: "./projects", title: "Projects" },
+        { url: "./resume", title: "Resume" },
+        { url: "./contact", title: "Contact" },
+        { url: "https://github.com/mass-ave", title: "GitHub" },
+    ];
+
+ 	let colorScheme = "light dark";
+ 	let root = globalThis?.document?.documentElement;
+ 	$: root?.style.setProperty("color-scheme", colorScheme);
+</script>
+{colorScheme}
+<label class="color-scheme">
+    Theme:
+   <select bind:value={ colorScheme }>
+        <option value="light dark">Automatic</option>
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+    </select>
+</label>
+<nav>
+    {#each pages as p}
+    <a
+    href={p.url}
+    class:current={"." + $page.route.id === p.url}
+    target={p.url.startsWith("http") ? "_blank" : null}
+    >
+    {p.title}
+    </a>
+
+    {/each}
+</nav>
+<style>
+    nav {
+      display: flex;
+      border-bottom-width: 1px;
+      border-bottom-style: solid;
+      border-bottom-color: oklch(50% 10% 200 / 40%);
+    }
+    
+    nav a {
+      flex: 1;
+      text-decoration: none;
+      color: inherit;
+      text-align: center;
+      padding: 0.5em;
+    }
+    
+    nav a.current {
+      border-bottom-width: 0.4em;
+      border-bottom-style: solid;
+      border-bottom-color: var(--color-accent);
+      padding-bottom: 0.5em;
+    }
+    
+    nav a:hover {
+      border-bottom-width: 0.4em;
+      border-bottom-style: solid;
+      border-bottom-color: var(--color-accent);
+      background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
+    }
+
+    label.color-scheme {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        font-size: 75%;
+        display: inline-flex;
+        gap: 0.5em;
+    }
+
+    label {
+    font: inherit;
+    }
+
+    label {
+    display: grid;
+    grid-template-columns: subgrid;
+    grid-column: 1 / -1;
+    }
+</style>
+<slot />
--- static/global.js
+++ static/global.js
@@ -2,50 +2,50 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let pages = [
-  { url: "./", title: "Home" },
-  { url: "./projects", title: "Projects" },
-  { url: "./resume", title: "Resume" },
-  { url: "./contact", title: "Contact" },
-  { url: "https://github.com/mass-ave", title: "GitHub" },
-];
+// let pages = [
+//   { url: "./", title: "Home" },
+//   { url: "./projects", title: "Projects" },
+//   { url: "./resume", title: "Resume" },
+//   { url: "./contact", title: "Contact" },
+//   { url: "https://github.com/mass-ave", title: "GitHub" },
+// ];
 
-let nav = document.createElement("nav");
-document.body.prepend(nav);
+// let nav = document.createElement("nav");
+// document.body.prepend(nav);
 
-for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  if (!url.startsWith("http")) {
-    url = `../${url}`;
-  }
-  let a = document.createElement("a");
-  a.href = url;
-  a.textContent = title;
+// for (let p of pages) {
+//   let url = p.url;
+//   let title = p.title;
+//   if (!url.startsWith("http")) {
+//     url = `../${url}`;
+//   }
+//   let a = document.createElement("a");
+//   a.href = url;
+//   a.textContent = title;
 
-  if (a.host === location.host && a.pathname === location.pathname) {
-    a.classList.add("current");
-  }
+//   if (a.host === location.host && a.pathname === location.pathname) {
+//     a.classList.add("current");
+//   }
 
-  if (a.host !== location.host) {
-    a.target = "_blank";
-  }
+//   if (a.host !== location.host) {
+//     a.target = "_blank";
+//   }
 
-  nav.append(a);
-}
+//   nav.append(a);
+// }
 
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  `
-	<label class="color-scheme">
-		Theme:
-		<select>
-			<option value="light dark">Automatic</option>
-            <option value="dark">Dark</option>
-            <option value="light">Light</option>
-		</select>
-	</label>`
-);
+// document.body.insertAdjacentHTML(
+//   "afterbegin",
+//   `
+// 	<label class="color-scheme">
+// 		Theme:
+// 		<select>
+// 			<option value="light dark">Automatic</option>
+//             <option value="dark">Dark</option>
+//             <option value="light">Light</option>
+// 		</select>
+// 	</label>`
+// );
 
 let select = document.querySelector("select");
 
