2920885927aef17cc7c95b57043bb007919b8c21
acevedo <acevedo@mit.edu>
2025-03-14 17:20:18 -0400

    trying something out

---
+++ src/lib/Pie.svelte
@@ -0,0 +1,62 @@
+<script>
+    import * as d3 from 'd3';
+
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let arc = arcGenerator({
+        startAngle: 0,
+        endAngle: 2 * Math.PI
+    });
+
+    let data = [
+        { value: 1, label: "apples" },
+        { value: 2, label: "oranges" },
+        { value: 3, label: "mangos" },
+        { value: 4, label: "pears" },
+        { value: 5, label: "limes" },
+        { value: 5, label: "cherries" }
+    ];
+    let sliceGenerator = d3.pie().value(d => d.value);
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map(d => arcGenerator(d));
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+</script>
+<svg viewBox="-50 -50 100 100">
+	{#each arcs as arc, index}
+        <path d={ arc } fill={ colors(index) } />
+    {/each}
+</svg>
+<ul class="legend">
+	{#each data as d, index}
+		<li style="--color: { colors(index) }">
+			<span class="swatch"></span>
+			{d.label} <em>({d.value})</em>
+		</li>
+	{/each}
+</ul>
+<style>
+    svg {
+        max-width: 20em;
+        margin-block: 2em;
+
+        /* Do not clip shapes outside the viewBox */
+        overflow: visible;
+    }
+
+    span {
+        display: inline-flex;
+        background-color: var(--color);
+        border-radius: 50%;
+    }
+
+    ul {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(10em, 1fr));
+        border: 1px solid black;
+        padding: 1em;
+    }
+
+    li {
+        display: flex;
+        align-items: center;
+    }
+</style>
