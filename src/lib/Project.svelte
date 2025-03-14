5c1356497ef278a28616687dd5109882256a563c
acevedo <acevedo@mit.edu>
2025-03-14 13:34:48 -0400

    added year to projects

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -5,11 +5,19 @@
 <article>
     <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
     <img src={data.image} alt="" />
-    <p>{data.description}</p>
+    <div>
+        <p>{data.description}</p>
+        <p class="year">Year: {data.year}</p>
+    </div>
 </article>
 <style>
     img {
         max-width: 100%;
         height: auto;
+    }
+
+    .year {
+        font-family: Baskerville;
+        font-variant-numeric: oldstyle-nums
     }
 </style>
