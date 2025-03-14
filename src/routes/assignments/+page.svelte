5903642338a82b02721a64263d5d66b488637a85
acevedo <acevedo@mit.edu>
2025-03-14 12:42:02 -0400

    deleted assignments

--- src/routes/assignments/+page.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-  <title>Assignments</title>
-</svelte:head>
-<h1>Assignments</h1>
-<p><b>A2 report:</b> <a href="assignments/A2">here</a></p>
-<p><b>A3 report:</b> <a href="assignments/A3">here</a></p>
