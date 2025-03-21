ad28c0f5f21f102512464011a7cd026c3a164ef1
acevedo <acevedo@mit.edu>
2025-03-20 23:04:21 -0400

    testing server

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,6 +1,15 @@
 <svelte:head>
   <title>Meta</title>
 </svelte:head>
+<script>
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
 
+  let data = [];
+
+  onMount(async () => {
+    data = await d3.csv("/loc.csv");
+  });
+</script>
 <h1>Meta</h1>
-<p>Something here</p>
+<p>Total lines of code: {data.length}</p>
