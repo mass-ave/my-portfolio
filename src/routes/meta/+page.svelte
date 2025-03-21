159099a39cc411059a1f7545d4131d82b7beaed8
acevedo <acevedo@mit.edu>
2025-03-20 23:09:45 -0400

    idk why the server aint working

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -8,7 +8,14 @@
   let data = [];
 
   onMount(async () => {
-    data = await d3.csv("/loc.csv");
+    data = await d3.csv("/loc.csv", row => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime)
+    }));
   });
 </script>
 <h1>Meta</h1>
