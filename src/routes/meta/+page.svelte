7375629e0693070bf791e72ac23c2bbc2e0eeabc
acevedo <acevedo@mit.edu>
2025-03-20 23:43:25 -0400

    back to basics

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -8,15 +8,19 @@
   let data = [];
 
   onMount(async () => {
-    data = await d3.csv("/loc.csv", row => ({
-      ...row,
-      line: Number(row.line), // or just +row.line
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + "T00:00" + row.timezone),
-      datetime: new Date(row.datetime)
-    }));
+    data = await d3.csv("/loc.csv");
   });
+
+  // onMount(async () => {
+  //   data = await d3.csv("/loc.csv", row => ({
+  //     ...row,
+  //     line: Number(row.line), // or just +row.line
+  //     depth: Number(row.depth),
+  //     length: Number(row.length),
+  //     date: new Date(row.date + "T00:00" + row.timezone),
+  //     datetime: new Date(row.datetime)
+  //   }));
+  // });
   console.log(data)
 </script>
 <h1>Meta</h1>
