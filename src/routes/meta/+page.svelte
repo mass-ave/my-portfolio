5233c039d2a47cb4e83aa0a8d09b3813f1e72852
acevedo <acevedo@mit.edu>
2025-03-20 23:46:49 -0400

    works in server

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -8,19 +8,15 @@
   let data = [];
 
   onMount(async () => {
-    data = await d3.csv("/loc.csv");
+    data = await d3.csv("/loc.csv", row => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime)
+    }));
   });
-
-  // onMount(async () => {
-  //   data = await d3.csv("/loc.csv", row => ({
-  //     ...row,
-  //     line: Number(row.line), // or just +row.line
-  //     depth: Number(row.depth),
-  //     length: Number(row.length),
-  //     date: new Date(row.date + "T00:00" + row.timezone),
-  //     datetime: new Date(row.datetime)
-  //   }));
-  // });
   console.log(data)
 </script>
 <h1>Meta</h1>
