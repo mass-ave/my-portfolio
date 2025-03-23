e10a017678d967dafb2fd38376e76bd707c20be9
acevedo <acevedo@mit.edu>
2025-03-22 23:59:10 -0400

    changed path to loc.csv

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -3,12 +3,13 @@
 </svelte:head>
 <script>
   import * as d3 from "d3";
+  import { base } from '$app/paths';
   import { onMount } from "svelte";
 
   let data = [];
 
   onMount(async () => {
-    data = await d3.csv("/loc.csv", row => ({
+    data = await d3.csv(`${base}/loc.csv`, row => ({
       ...row,
       line: Number(row.line), // or just +row.line
       depth: Number(row.depth),
