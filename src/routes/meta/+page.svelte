991c3cf58c758bb614fba830710d030c72d29c01
acevedo <acevedo@mit.edu>
2025-03-28 23:46:43 -0400

    lab7.2 done

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -41,6 +41,48 @@
     });
   });
   const authors = d3.groups(data, d => d.author).length + 1;
+
+  let width = 1000, height = 600;
+
+  $: minDate = d3.min(commits.map(d => d.date));
+  $: maxDate = d3.max(commits.map(d => d.date));
+  $: maxDatePlusOne = new Date(maxDate);
+  $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+  $: xScale = d3.scaleTime()
+                .domain([minDate, maxDatePlusOne])
+                .range([0, width])
+                .nice();
+
+  $: yScale = d3.scaleLinear()
+                .domain([24, 0])
+                .range([height, 0]);
+
+  let margin = {top: 10, right: 0, bottom: 0, left: 0};
+  let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left
+  };
+  usableArea.width = usableArea.right - usableArea.left;
+  usableArea.height = usableArea.bottom - usableArea.top;
+
+  let xAxis, yAxis;
+  $: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
+  }
+
+  let yAxisGridlines;
+  $: {
+    d3.select(yAxisGridlines).call(
+      d3.axisLeft(yScale)
+        .tickFormat("")
+        .tickSize(-usableArea.width)
+    );
+  }
+
 </script>
 <h1>Meta</h1>
 <dl class="stats">
@@ -51,3 +93,28 @@
   <dt>Number of authors:</dt>
 	<dd>{authors}</dd>
 </dl>
+<svg viewBox="0 0 {width} {height}">
+  <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+  <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+	<g class="dots">
+    {#each commits as commit, index }
+      <circle
+        cx={ xScale(commit.datetime) }
+        cy={ yScale(commit.hourFrac) }
+        r="5"
+        fill="steelblue"
+      />
+    {/each}
+  </g>
+</svg>
+<style>
+	svg {
+		overflow: visible;
+    padding: 1em;
+	}
+
+  .gridlines {
+    stroke-opacity: .2;
+  }
+</style>
